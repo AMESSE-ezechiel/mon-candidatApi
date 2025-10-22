26a53a1b332635233cf661391be9f8714a49f546
TAKOUDA Eyana Mario <mariotakouda99gmail.com>
2025-10-22 10:19:45 +0200

    new commit

--- database/migrations/2025_10_22_093200_create_candidates_table.php
+++ database/migrations/2025_10_22_093200_create_candidates_table.php
@@ -6,20 +6,24 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+   
     public function up(): void
     {
         Schema::create('candidates', function (Blueprint $table) {
             $table->id();
+            $table->string('nom', 100);
+            $table->string('prenom', 100);
+            $table->string('nationalite', 100)->nullable();
+            $table->unsignedTinyInteger('age')->nullable();
+            $table->decimal('poids', 5, 2)->nullable();
+            $table->decimal('taille', 5, 2)->nullable();
+            $table->string('short_description', 255)->nullable();
+            $table->text('full_description')->nullable();
+            $table->string('photo_path')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('candidates');
